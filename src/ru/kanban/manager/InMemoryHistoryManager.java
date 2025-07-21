607a25c6cdfa846a85a78f911881d913c4b58048
Bogdan Frolov / gdouglas <frolovb880@gmail.com>
2025-07-21 13:53:16 +0300

    Moved Node class to a separate file

--- src/ru/kanban/manager/InMemoryHistoryManager.java
+++ src/ru/kanban/manager/InMemoryHistoryManager.java
@@ -7,39 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-class Node {
-    private final Task task;
-    private Node prev;
-    private Node next;
-
-    public Node(Node prev, Task task, Node next) {
-        this.task = task;
-        this.prev = prev;
-        this.next = next;
-    }
-
-    public Task getTask() {
-        return task;
-    }
-
-    public Node getPrev() {
-        return prev;
-    }
-
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-}
-
-
 public class InMemoryHistoryManager implements HistoryManager {
 
     private static final int MAX_SIZE = 10;
