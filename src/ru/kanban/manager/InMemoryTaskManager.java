8418fc0fa13f8e58ef38247df00a7c30916e2726
Bogdan Frolov / gdouglas <frolovb880@gmail.com>
2025-07-29 22:04:07 +0300

    Fixed style

--- src/ru/kanban/manager/InMemoryTaskManager.java
+++ src/ru/kanban/manager/InMemoryTaskManager.java
@@ -10,9 +10,9 @@ import java.util.List;
 import java.util.Map;
 
 public class InMemoryTaskManager implements TaskManager {
-     protected int idCounter = 0;
+    protected int idCounter = 0;
 
-    protected  final Map<Integer, Task> tasks = new HashMap<>();
+    protected final Map<Integer, Task> tasks = new HashMap<>();
     protected final Map<Integer, Epic> epics = new HashMap<>();
     protected final Map<Integer, SubTask> subtasks = new HashMap<>();
     protected final HistoryManager historyManager;
